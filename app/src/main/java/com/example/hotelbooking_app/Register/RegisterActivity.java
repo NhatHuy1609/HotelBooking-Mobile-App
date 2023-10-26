f88eddd78c0d117e7bccfcee83448b564209e6e2
huan <phbinhthuongtam@gmail.com>
2023-10-26 08:28:25 +0700

    Merge remote-tracking branch 'origin/phanphuochuan_branch' into phanphuochuan_branch

--- app/src/main/java/com/example/hotelbooking_app/Register/RegisterActivity.java
+++ app/src/main/java/com/example/hotelbooking_app/Register/RegisterActivity.java
@@ -7,8 +7,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.hotelbooking_app.AdditionalProfile.AdditionalProfileActivity;
+import com.example.hotelbooking_app.Login.Activity.LoginActivity;
 import com.example.hotelbooking_app.R;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -26,6 +28,15 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(RegisterActivity.this, AdditionalProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        TextView textView=findViewById(R.id.signup_move_login);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(RegisterActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
