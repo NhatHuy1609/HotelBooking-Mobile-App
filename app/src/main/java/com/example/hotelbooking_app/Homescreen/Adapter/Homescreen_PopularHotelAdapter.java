865e74ebc41a3a54cc5b37a7955446ec32a87e78
tdn283 <nhatflame@gmail.com>
2023-10-27 12:58:12 +0700

    code intent from Home to Detail

--- app/src/main/java/com/example/hotelbooking_app/Homescreen/Adapter/Homescreen_PopularHotelAdapter.java
+++ app/src/main/java/com/example/hotelbooking_app/Homescreen/Adapter/Homescreen_PopularHotelAdapter.java
@@ -5,20 +5,28 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import com.example.hotelbooking_app.Homescreen.Hotels.Homescreen_PopularHotel;
 
 import com.example.hotelbooking_app.R;
 import com.example.hotelbooking_app.Searching.Activity.DetailActivity;
+import com.example.hotelbooking_app.Searching.Adapter.RecentlyViewedAdapter;
 
 import java.util.List;
 
 public class Homescreen_PopularHotelAdapter extends BaseAdapter {
+    OnItemClickListener onItemClickListener;
 
     private Context context;
     private int layout;
     private List<Homescreen_PopularHotel> popularHotelList;
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
+    }
 
     public Homescreen_PopularHotelAdapter(Context context, int layout, List<Homescreen_PopularHotel> popularHotelList) {
         this.context = context;
@@ -41,6 +49,10 @@ public class Homescreen_PopularHotelAdapter extends BaseAdapter {
         return 0;
     }
 
+    public interface OnItemClickListener {
+        void onItemClick(int position);
+    }
+
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
 
@@ -55,6 +67,8 @@ public class Homescreen_PopularHotelAdapter extends BaseAdapter {
         TextView txtSLDanhGia = (TextView) view.findViewById(R.id.home_SLdanhgia_popularhotel);
         TextView txtGia = (TextView) view.findViewById(R.id.home_price_popularhotel);
 
+        RelativeLayout pplHotelItem = (RelativeLayout) view.findViewById(R.id.homescreen_item_popular_hotel);
+
         //gán giá trị
         Homescreen_PopularHotel popularHotel = popularHotelList.get(i);
 
@@ -64,6 +78,16 @@ public class Homescreen_PopularHotelAdapter extends BaseAdapter {
         txtDanhGia.setText(String.valueOf(popularHotel.getDanhGia()));
         txtSLDanhGia.setText(String.valueOf(popularHotel.getSoLuongDanhGia()));
         txtGia.setText(popularHotel.getGia());
+
+        pplHotelItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (onItemClickListener != null) {
+                    onItemClickListener.onItemClick(i);
+                }
+            }
+        });
+
         return view;
     }
 
