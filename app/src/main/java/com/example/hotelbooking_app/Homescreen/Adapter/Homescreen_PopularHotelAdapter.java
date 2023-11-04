e9c4272f69056e0ff25702e63d9e216060c71306
pvl <phanvanlinh261003@gmail.com>
2023-11-05 06:03:13 +0700

    update click tym on popular item hotel

--- app/src/main/java/com/example/hotelbooking_app/Homescreen/Adapter/Homescreen_PopularHotelAdapter.java
+++ app/src/main/java/com/example/hotelbooking_app/Homescreen/Adapter/Homescreen_PopularHotelAdapter.java
@@ -1,37 +1,31 @@
 package com.example.hotelbooking_app.Homescreen.Adapter;
+
 import android.content.Context;
-import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import com.example.hotelbooking_app.Homescreen.Hotels.Homescreen_PopularHotel;
-
 import com.example.hotelbooking_app.R;
-import com.example.hotelbooking_app.Searching.Activity.DetailActivity;
-import com.example.hotelbooking_app.Searching.Adapter.RecentlyViewedAdapter;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Homescreen_PopularHotelAdapter extends BaseAdapter {
-//    OnItemClickListener onItemClickListener;
 
     private Context context;
     private int layout;
     private List<Homescreen_PopularHotel> popularHotelList;
-
-//    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
-//        this.onItemClickListener = onItemClickListener;
-//    }
+    private List<Boolean> isRedHeartList;
 
     public Homescreen_PopularHotelAdapter(Context context, int layout, List<Homescreen_PopularHotel> popularHotelList) {
         this.context = context;
         this.layout = layout;
         this.popularHotelList = popularHotelList;
+        isRedHeartList = new ArrayList<>(Collections.nCopies(popularHotelList.size(), false));
     }
 
     @Override
@@ -41,35 +35,31 @@ public class Homescreen_PopularHotelAdapter extends BaseAdapter {
 
     @Override
     public Object getItem(int i) {
-        return null;
+        return popularHotelList.get(i);
     }
 
     @Override
     public long getItemId(int i) {
-        return 0;
+        return i;
     }
 
-//    public interface OnItemClickListener {
-//        void onItemClick(int position);
-//    }
-
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
+    public View getView(final int i, View view, ViewGroup viewGroup) {
+        if (view == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(layout, null);
+        }
 
-        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        view = inflater.inflate(layout, null);
+        // Ánh xạ view
+        TextView txtTen = view.findViewById(R.id.home_name_popularhotel);
+        TextView txtDiaChi = view.findViewById(R.id.home_location_popularhotel);
+        ImageView imgHinh = view.findViewById(R.id.home_img_popularhotel);
+        TextView txtDanhGia = view.findViewById(R.id.home_rate_popularhotel);
+        TextView txtSLDanhGia = view.findViewById(R.id.home_SLdanhgia_popularhotel);
+        TextView txtGia = view.findViewById(R.id.home_price_popularhotel);
+        ImageView heartImageView = view.findViewById(R.id.home_tym);
 
-        // ánh xạ view
-        TextView txtTen = (TextView) view.findViewById(R.id.home_name_popularhotel);
-        TextView txtDiaChi = (TextView) view.findViewById(R.id.home_location_popularhotel);
-        ImageView imgHinh = (ImageView) view.findViewById(R.id.home_img_popularhotel);
-        TextView txtDanhGia = (TextView) view.findViewById(R.id.home_rate_popularhotel);
-        TextView txtSLDanhGia = (TextView) view.findViewById(R.id.home_SLdanhgia_popularhotel);
-        TextView txtGia = (TextView) view.findViewById(R.id.home_price_popularhotel);
-
-//        RelativeLayout pplHotelItem = (RelativeLayout) view.findViewById(R.id.homescreen_item_popular_hotel);
-
-        //gán giá trị
+        // Gán giá trị
         Homescreen_PopularHotel popularHotel = popularHotelList.get(i);
 
         txtTen.setText(popularHotel.getTen());
@@ -79,17 +69,29 @@ public class Homescreen_PopularHotelAdapter extends BaseAdapter {
         txtSLDanhGia.setText(String.valueOf(popularHotel.getSoLuongDanhGia()));
         txtGia.setText(popularHotel.getGia());
 
-//        pplHotelItem.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if (onItemClickListener != null) {
-//                    onItemClickListener.onItemClick(i);
-//                }
-//            }
-//        });
+        // Đổi màu tim
+        if (isRedHeartList.get(i)) {
+            heartImageView.setImageResource(R.drawable.homescreen_heart_red);
+        } else {
+            heartImageView.setImageResource(R.drawable.homescreen_heart_white);
+        }
+
+        // Xử lý sự kiện khi click vào hình trái tim
+        heartImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Đảo ngược trạng thái trái tim khi được click
+                isRedHeartList.set(i, !isRedHeartList.get(i));
+
+                // Cập nhật hình trái tim dựa trên trạng thái mới
+                if (isRedHeartList.get(i)) {
+                    heartImageView.setImageResource(R.drawable.homescreen_heart_red);
+                } else {
+                    heartImageView.setImageResource(R.drawable.homescreen_heart_white);
+                }
+            }
+        });
 
         return view;
     }
-
 }
-
